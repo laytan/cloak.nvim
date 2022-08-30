44439f5fb349a07d026eca29e03b68163ef94c7b
Laytan Laats <laytan.laats@republic-m.com>
2022-08-30 14:58:33 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,38 @@
+# Cloak.nvim
+
+Cloak allows you to overlay *'s over defined patterns in defined files.
+
+It also disabled 'cmp' for the buffer(if it is installed).
+
+## Demo
+
+![Demo](https://user-images.githubusercontent.com/20369598/187440609-4cfce257-a4c2-4036-8ad7-3f3bb583e994.gif)
+
+## Configuration
+
+Here is the default configuration.
+
+'file_pattern' can be a string or table of strings, they should be valid autocommand patterns.
+'cloak_pattern' is a lua pattern ran over every line in the buffer, overlaying a * over the match, excluding the first character.
+
+```lua
+require('cloak').setup({
+  enabled = true,
+  patterns = {
+    {
+      -- Match any file starting with '.env'.
+      file_pattern = '.env*',
+      -- Match an equals sign and any character after it.
+      cloak_pattern = '=.+'
+    },
+  },
+})
+```
+
+## Usage
+
+The plugin automatically activates when a file mathed by the patterns is opened.
+
+You do have to call the 'setup()' function.
+
+':CloakDisable' and ':CloakEnable' are also available to toggle cloaking.
---
+++ lua/cloak/init.lua
@@ -0,0 +1,70 @@
+local group = vim.api.nvim_create_augroup('cloak', {})
+local namespace = vim.api.nvim_create_namespace('cloak')
+
+local has_cmp, cmp = pcall(require, 'cmp')
+
+local M = {}
+
+M.opts = {
+  enabled = true,
+  patterns = { { file_pattern = '.env*', cloak_pattern = '=.+' } },
+}
+
+M.setup = function(opts)
+  M.opts = vim.tbl_deep_extend('force', M.opts, opts or {})
+
+  for _, pattern in ipairs(M.opts.patterns) do
+    vim.api.nvim_create_autocmd(
+      { 'BufRead', 'TextChanged', 'TextChangedI', 'TextChangedP' }, {
+        pattern = pattern.file_pattern,
+        callback = function()
+          if M.opts.enabled then
+            M.cloak(pattern.cloak_pattern)
+          end
+        end,
+        group = group,
+      }
+    )
+  end
+
+  vim.api.nvim_create_user_command('CloakEnable', M.enable, {})
+  vim.api.nvim_create_user_command('CloakDisable', M.disable, {})
+end
+
+M.uncloak = function()
+  vim.api.nvim_buf_clear_namespace(0, namespace, 0, -1)
+end
+
+M.cloak = function(cloak_pattern)
+  M.uncloak()
+
+  if has_cmp then
+    cmp.setup.buffer({ enabled = false })
+  end
+
+  local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+  for i, line in ipairs(lines) do
+    local first, last = line:find(cloak_pattern)
+
+    if first ~= nil then
+      vim.api.nvim_buf_set_extmark(
+        0, namespace, i - 1, first, {
+          virt_text = { { string.rep('*', last - first), 'Comment' } },
+          virt_text_pos = 'overlay',
+        }
+      )
+    end
+  end
+end
+
+M.disable = function()
+  M.uncloak()
+  M.opts.enabled = false
+end
+
+M.enable = function()
+  M.opts.enabled = true
+  vim.cmd('doautocmd TextChanged')
+end
+
+return M
